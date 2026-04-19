5df7fae406a111545297d775dd60db9b86521372
MuskanParmar-mp <parmarmuskan1362006@gmail.com>
2026-04-19 22:00:00 +0530

    minimilist

--- src/Wrinkles.jsx
+++ src/Wrinkles.jsx
@@ -181,7 +181,7 @@ function Wrinkles() {
               />
               <div style={{marginLeft:"-43px", marginTop:"-110px"}}> 
               <h2  className=" text-black mt-10 text-" style={{marginTop:"80px", fontSize:"20px" , marginBottom:"10px" }}>
-              Granactive Retinoid 2% Face Cream
+              Granactive Retinoid 2% Face Creame
               </h2>
 
               <p className="text-gray-500 text-sm " style={{marginBottom:"18px",}}>
