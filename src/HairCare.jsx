4aa33c9c9c77e2eb3e5182aca2b99a142a134791
MuskanParmar-mp <parmarmuskan1362006@gmail.com>
2026-04-29 20:57:53 +0530

    minimilist

--- src/HairCare.jsx
+++ src/HairCare.jsx
@@ -555,7 +555,7 @@ function HairCare() {
               </div>
 
               
-              <p>₹789 <span className="line-through text-gray-400">₹799</span></p>
+              <p>₹744 <span className="line-through text-gray-400">₹799</span></p>
 
               <button
                 onClick={() => setOpenIndex(openIndex === 0 ? null : 0)}
